78a627d390dc14ef6b5fab2fdedc4db6776e050c
zbluee <amanueltemesgen9@gmail.com>
2023-04-04 04:48:50 -0400

    feat: add clear, search and remove with index method to DoublyLinkedList class.

--- Linked List/doubleLinkedList.py
+++ Linked List/doubleLinkedList.py
@@ -85,20 +85,60 @@ class DoublyLinkedList:
         self.head.prev = None
         self.number_of_nodes -= 1
 
+    def remove_at_end(self) -> None:
+        if self.head is None:
+            raise IndexError("remove from empty list")
 
-if __name__ == "__main__":
-    dll = DoublyLinkedList()
-    dll.insert_at_beg(4)
-    dll.insert_at_beg(3)
-    dll.insert_at_beg(2)
-    dll.insert_at_beg(1)
-    dll.insert_at_end(5)
-    dll.insert_at_end(6)
-    dll.insert(0, 0)
-    dll.insert(2.5, 3)
-    dll.remove_at_beg()
-    dll.remove_at_beg()
-    dll.remove_at_beg()
-    dll.remove_at_beg()
-    print([data for data in dll], len(dll))
+        if self.head == self.tail:
+            self.remove_at_beg()
+            return
+
+        self.tail = self.tail.prev
+        self.tail.next = None
+        self.number_of_nodes -= 1
+
+    def remove(self, index : int) -> None:
+        if index == 0:
+            self.remove_at_beg()
+            return
+
+        if index == self.number_of_nodes - 1:
+            self.remove_at_end()
+            return
+
+        if self.head is None:
+            raise IndexError("remove from empty list")
+
+        if index < 0 or index >= self.number_of_nodes:
+            raise IndexError("index out of bound.")
+
+        itr = self.head
+        while index - 1 > 0:
+            itr = itr.next
+            index -= 1
+
+        itr.next = itr.next.next
+        itr.next.prev = itr
+        self.number_of_nodes -= 1
+
+    def search(self, value) -> bool:
+        itr = self.head
+        while itr:
+            if itr.data == value:
+                return True
+            itr = itr.next
+
+        return False
+
+    def clear(self) -> None:
+        if self.head is not None:
+            itr = self.head
+            while itr:
+                itr.prev = None
+                itr = itr.next
+
+            self.head = None
+            self.tail = None
+            self.number_of_nodes = 0
+
 
