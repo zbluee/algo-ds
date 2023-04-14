a1b0e72241f6ae2208bd05e253d367e2429d7a92
zbluee <amanueltemesgen9@gmail.com>
2023-04-14 01:37:35 -0500

    feat: add node and linkedlist classes.

---
+++ Stack/stackLinkedList.py
@@ -0,0 +1,13 @@
+
+class Node:
+    def __init__(self, data = None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.nodes = 0
+
+
