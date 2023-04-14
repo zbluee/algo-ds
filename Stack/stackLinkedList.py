9bafe28175d91043cb7ae93dd325e9690aa63a25
zbluee <amanueltemesgen9@gmail.com>
2023-04-14 01:52:31 -0500

    feat: add isempty, push and other dunder methods to stack class.

--- Stack/stackLinkedList.py
+++ Stack/stackLinkedList.py
@@ -11,3 +11,32 @@ class LinkedList:
         self.nodes = 0
 
 
+class Stack:
+    def __init__(self):
+        self.linkedList = LinkedList()
+
+    def __iter__(self):
+        itr = self.linkedList.head
+        while itr:
+            yield itr.data
+            itr = itr.next
+
+    def __len__(self):
+        return self.linkedList.nodes
+
+    def is_empty(self):
+        return self.linkedList.head == None
+
+    def push(self, data) -> None:
+        node = Node(data)
+        node.next = self.linkedList.head
+        self.linkedList.head = node
+        self.linkedList.nodes += 1
+
+
+stack = Stack()
+stack.push(5)
+stack.push(4)
+stack.push(3)
+print(stack.is_empty())
+print([v for v in stack], len(stack))
