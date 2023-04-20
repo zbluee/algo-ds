469073067c6d72ce17d23624cebae95ce79eb9ab
zbluee <amanueltemesgen9@gmail.com>
2023-04-20 01:39:42 -0500

    feat: add isempty and enqueue methods to Queue class.

--- Queue/queueLinkedList.py
+++ Queue/queueLinkedList.py
@@ -14,6 +14,36 @@ class LinkedList:
 
 class Queue:
     def __init__(self):
-        self.LinkedList = LinkedList()
+        self.linkedList = LinkedList()
+
+    def __iter__(self):
+        itr = self.linkedList.head
+        while itr:
+            yield itr.data
+            itr = itr.next
+
+    def __len__(self):
+        return self.linkedList.nodes
+
+    def is_empty(self) -> bool:
+        return self.linkedList.head == None
+
+    def enqueue(self, data) -> None:
+        node = Node(data)
+        if self.is_empty():
+            self.linkedList.head = node
+            self.linkedList.tail = node
+            self.linkedList.nodes += 1
+            return
+
+        self.linkedList.tail.next = node
+        self.linkedList.tail = node
+        self.linkedList.nodes += 1
 
 
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+print([v for v in q], len(q))
