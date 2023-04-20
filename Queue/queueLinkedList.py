92f33b1263a5cecf018f29f7ea704fed94d7b5ab
zbluee <amanueltemesgen9@gmail.com>
2023-04-20 01:31:24 -0500

    feat: add node, linkedlist and queue classes.

---
+++ Queue/queueLinkedList.py
@@ -0,0 +1,19 @@
+
+class Node:
+    def __init__(self, data = None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.nodes = 0
+
+
+class Queue:
+    def __init__(self):
+        self.LinkedList = LinkedList()
+
+
