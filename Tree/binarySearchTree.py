4bc9e1d49835a351794ff2b35bc196403508691a
zbluee <amanueltemesgen9@gmail.com>
2023-05-06 07:58:33 -0500

    feat: add len, get min and max node to a BST class.

--- Tree/binarySearchTree.py
+++ Tree/binarySearchTree.py
@@ -26,4 +26,27 @@ class BST:
         self.root = None
         self.nodes = 0
 
+    def __len__(self):
+        return self.nodes
+
+    def get_min_node(self, root = None) -> BSTNode:
+        """
+        Get the node with the minimum value in the BST.
+
+        """
+        current_node = root if root else self.root
+        while current_node.left:
+            current_node = current_node.left
+        return current_node
+
+    def get_max_node(self, root = None) -> BSTNode:
+        """
+        Get the node with the maximum value in the BST.
+
+        """
+        current_node = root if root else self.root
+        while current_node.right:
+            current_node = current_node.right
+        return current_node
+
 
